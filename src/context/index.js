d6f67cfde40d8a9aa57694c8885df5742e1f5e60
Kent C. Dodds <me+github@kentcdodds.com>
2020-04-10 16:07:09 -0600

    exercises/07-context

--- src/context/index.js
+++ src/context/index.js
@@ -1,22 +1,6 @@
-import React from 'react'
-import {BrowserRouter as Router} from 'react-router-dom'
-import {ReactQueryConfigProvider} from 'react-query'
-import {homepage} from '../../package.json'
-import {AuthProvider} from './auth-context'
+export * from './index.final'
 
-const queryConfig = {
-  useErrorBoundary: true,
-  refetchAllOnWindowFocus: false,
-}
+// export * from './index.exercise'
 
-function AppProviders({children}) {
-  return (
-    <ReactQueryConfigProvider config={queryConfig}>
-      <Router basename={homepage}>
-        <AuthProvider>{children}</AuthProvider>
-      </Router>
-    </ReactQueryConfigProvider>
-  )
-}
-
-export {AppProviders}
+// 💯 colocate global providers
+// export * from './index.extra-2'
